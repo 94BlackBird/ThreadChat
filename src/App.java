d861f023a58c3417d69a5adedea92960e75b0a98
Roman Bondarev <bondarev.roma.94@gmail.com>
2021-11-10 12:01:32 +0300

    init

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int port1 = Integer.parseInt(args[0]);
+        int port2 = Integer.parseInt(args[1]);
+       
+        System.out.println("Starting chat App...");
+
+        ChatServer chatSrv = new ChatServer(port1);
+        ChatClient chatCl = new ChatClient(port2);
+
+        chatSrv.start();
+        chatCl.start();
+    }
+}
